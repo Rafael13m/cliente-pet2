4ffb4adcb36719588ca5733373135e14960ef58c
Rafael13m <rafael13m@hotmail.com>
2026-02-22 02:21:09 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteResponse.java
@@ -1,7 +1,12 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
 public class ClienteResponse {
 
-
+    private UUID idCliente;
 
 }
