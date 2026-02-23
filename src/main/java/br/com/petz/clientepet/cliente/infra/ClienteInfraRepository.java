f67cecda29a0dc2bcbc9a4d301615039c2631839
Rafael13m <rafael13m@hotmail.com>
2026-02-22 22:11:27 -0300

    Handler implementado

--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
@@ -3,8 +3,10 @@ package br.com.petz.clientepet.cliente.infra;
 import br.com.petz.clientepet.cliente.application.api.ClienteListResponse;
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import br.com.petz.clientepet.cliente.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -37,7 +39,7 @@ public class ClienteInfraRepository implements ClienteRepository {
     public Cliente buscaClientePorId(UUID idCliente) {
         log.info("Inicia Cliente Infra - Busca CLiente por id");
         Cliente cliente = clienteSpringDataJPARepository.findById(idCliente).
-                orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+                orElseThrow(() -> APIException.build(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
         log.info("Finaliza Cliente Infra - Busca CLiente por id");
         return cliente;
     }
